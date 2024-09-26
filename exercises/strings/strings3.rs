05b91b9de59be5be6792aca474e56396162c7a02
RaphaelNY <1278173058@qq.com>
2024-09-27 00:01:16 +0800

    update

--- exercises/strings/strings3.rs
+++ exercises/strings/strings3.rs
@@ -3,43 +3,58 @@
 // Execute `rustlings hint strings3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 fn trim_me(input: &str) -> String {
-    // TODO: Remove whitespace from both ends of a string!
-    ???
+	// TODO: Remove whitespace from both ends of a string!
+	// input.trim().to_string()
+	let start = input.chars().position(|c| !c.is_whitespace()).unwrap();
+	let end = input.chars().rev().position(|c| !c.is_whitespace()).unwrap();
+	input[start..input.len() - end].to_string()
 }
 
 fn compose_me(input: &str) -> String {
-    // TODO: Add " world!" to the string! There's multiple ways to do this!
-    ???
+	// TODO: Add " world!" to the string! There's multiple ways to do this!
+	input.to_string() + " world!"
 }
 
 fn replace_me(input: &str) -> String {
-    // TODO: Replace "cars" in the string with "balloons"!
-    ???
+	// TODO: Replace "cars" in the string with "balloons"!
+	// input.replace("cars", "balloons")
+	let target = "cars";
+	let replacement = "balloons";
+
+	let mut result = String::new();
+	let mut start = 0;
+
+	while let Some(pos) = input[start..].find(target) {
+		result.push_str(&input[start..start + pos]);
+		result.push_str(replacement);
+		start += pos + target.len();
+	}
+
+	result.push_str(&input[start..]);
+	result
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+	use super::*;
 
-    #[test]
-    fn trim_a_string() {
-        assert_eq!(trim_me("Hello!     "), "Hello!");
-        assert_eq!(trim_me("  What's up!"), "What's up!");
-        assert_eq!(trim_me("   Hola!  "), "Hola!");
-    }
+	#[test]
+	fn trim_a_string() {
+		assert_eq!(trim_me("Hello!     "), "Hello!");
+		assert_eq!(trim_me("  What's up!"), "What's up!");
+		assert_eq!(trim_me("   Hola!  "), "Hola!");
+	}
 
-    #[test]
-    fn compose_a_string() {
-        assert_eq!(compose_me("Hello"), "Hello world!");
-        assert_eq!(compose_me("Goodbye"), "Goodbye world!");
-    }
+	#[test]
+	fn compose_a_string() {
+		assert_eq!(compose_me("Hello"), "Hello world!");
+		assert_eq!(compose_me("Goodbye"), "Goodbye world!");
+	}
 
-    #[test]
-    fn replace_a_string() {
-        assert_eq!(replace_me("I think cars are cool"), "I think balloons are cool");
-        assert_eq!(replace_me("I love to look at cars"), "I love to look at balloons");
-    }
+	#[test]
+	fn replace_a_string() {
+		assert_eq!(replace_me("I think cars are cool"), "I think balloons are cool");
+		assert_eq!(replace_me("I love to look at cars"), "I love to look at balloons");
+	}
 }
